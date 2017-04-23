f4cdc13ae448b36eae563ec6a1f17aebb2f216b8
oNguyenVanThieu <nguyen.van.thieub@framgia.com>
2017-04-23 23:09:35 +0700

    Choose Answer

--- AiLaTrieuPhuTiengAnh/ViewController.swift
+++ AiLaTrieuPhuTiengAnh/ViewController.swift
@@ -9,25 +9,55 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    enum ANSWER: Int {
+        case A = 0
+        case B = 1
+        case C = 2
+        case D = 3
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
     }
 
     @IBAction func clickAnswerA(_ sender: Any) {
-        print("Answer A")
+        choose(answer: ANSWER.A)
     }
 
     @IBAction func clickAnswerB(_ sender: Any) {
-        print("Answer B")
+        choose(answer: ANSWER.B)
     }
     
     @IBAction func clickAnswerC(_ sender: Any) {
-        print("Answer C")
+        choose(answer: ANSWER.C)
     }
     
     @IBAction func clickAnswerD(_ sender: Any) {
-        print("Answer D")
+        choose(answer: ANSWER.D)
+    }
+    
+    private func choose(answer: ANSWER) {
+        // TODO: This is a sample data for test
+        showAnswer(answer == ANSWER.C)
+    }
+    
+    private func showAnswer(_ isCorrect: Bool) {
+        if isCorrect {
+            showTrue()
+        } else {
+            showFail()
+        }
+    }
+    
+    private func showTrue() {
+        // TODO: do something when correct
+        print("True")
+    }
+    
+    private func showFail() {
+        // TODO: do something when not correct
+        print("False")
     }
     
 }
