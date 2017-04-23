f1c0bc46682b0baed6a2560526134ca088f14be4
Nguyen Van Thieu <nguyen.van.thieub>
2017-04-23 22:38:27 +0700

    Init Project

---
+++ AiLaTrieuPhuTiengAnh/ViewController.swift
@@ -0,0 +1,34 @@
+//
+//  ViewController.swift
+//  AiLaTrieuPhuTiengAnh
+//
+//  Created by Thieu Mao on 4/23/17.
+//  Copyright © 2017 Hay Nhanh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    @IBAction func clickAnswerA(_ sender: Any) {
+        print("Answer A")
+    }
+
+    @IBAction func clickAnswerB(_ sender: Any) {
+        print("Answer B")
+    }
+    
+    @IBAction func clickAnswerC(_ sender: Any) {
+        print("Answer C")
+    }
+    
+    @IBAction func clickAnswerD(_ sender: Any) {
+        print("Answer D")
+    }
+    
+}
+
